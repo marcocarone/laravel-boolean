009f23d96e2e8136b658bd8091801912de101606
Marco <marco.carone@libero.it>
2020-03-17 15:34:22 +0100

    creazione rotte e file view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/studenti/', 'StudenteController@index')->name("studente.index");
+Route::get('/studenti/show/', 'StudenteController@show')->name("studente.show");
