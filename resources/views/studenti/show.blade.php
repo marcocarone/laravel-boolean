e735eea6c55d4a5f25f2d4e59effad003c34c91b
Marco <marco.carone@libero.it>
2020-03-17 18:05:18 +0100

    aggiornamento pagina studente

--- resources/views/studenti/show.blade.php
+++ resources/views/studenti/show.blade.php
@@ -1,1 +1,18 @@
-@dd($studente);
+
+
+@extends('layouts.layout')
+
+@section('main')
+
+
+<div class="box-studenti">
+    <img src="{{$studente["img"]}}" alt="{{$studente["nome"]}}">
+
+    <h2>{{$studente["nome"]}} ( {{$studente["eta"]}} anni)</h2>
+
+    <h3>Assunt{{($studente["genere"] == "m") ? "o" : "a"}} da {{$studente["azienda"]}} come {{$studente["ruolo"]}}</h3>
+    <p>{{$studente["descrizione"]}}</p>
+</div>
+
+
+@endsection
