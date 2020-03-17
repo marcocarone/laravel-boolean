86301de9182f04a0fe6742d9a5724d8eb1c9ee50
Marco <marco.carone@libero.it>
2020-03-17 16:18:25 +0100

    creato file nella cartella config dove ho inserito l'array degli studenti

---
+++ config/studenti.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+"studenti" => [
+  [
+    "img" => "https://www.boolean.careers/images/students/biagini.png",
+    "nome" => "Alessandro Biagini",
+    "eta" => 25,
+    "azienda" => "DISC SPA",
+    "ruolo" => "web developer",
+    "descrizione" => "Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.",
+    "genere" => "m",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/poggini.png",
+    "nome" => "Paola Poggini",
+    "eta" => 24,
+    "azienda" => "Prima Assicurazioni",
+    "ruolo" => "junior software engineer",
+    "descrizione" => "A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.",
+    "genere" => "f",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/masetti.png",
+    "nome" => "Loana Masetti",
+    "eta" => 36,
+    "azienda" => "The Zen Agency",
+    "ruolo" => "web developer",
+    "descrizione" => "Ha scoperto la passione per l'informatica creando un blog di psicologia, ambito in cui si era specializzata durante gli studi. Il suo cuore però non vuol sentir ragione e Loana cambia carriera e diventa una ricercatissima sviluppatrice web.",
+    "genere" => "f",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/patruno.png",
+    "nome" => "Davide Patruno",
+    "eta" => 29,
+    "azienda" => "ArmadioVerde",
+    "ruolo" => "web developer",
+    "descrizione" => "Dopo la laurea magistrale in scienze umanistiche e sociali, si concentra sullo sviluppo di strategie marketing. Decide di cambiare vita, questa volta puntando sullo sviluppo Front-End.",
+    "genere" => "m",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/castellani.png",
+    "nome" => "Tommaso Castellani",
+    "eta" => 30,
+    "azienda" => "Pearch Digital",
+    "ruolo" => "data scientist",
+    "descrizione" => "Un passato da giocatore professionista di poker e una laurea in ingegneria gestionale alle spalle. Tommaso ha tramutato la sua passione per i numeri in un lavoro diventando data scientist a Dublino.",
+    "genere" => "m",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/antidormi.png",
+    "nome" => "Nicholas Antidormi",
+    "eta" => 20,
+    "azienda" => "Atoms Studio",
+    "ruolo" => "web developer",
+    "descrizione" => "Intraprende gli studi universitari in matematica per le sue doti analitiche, ma abbandona il percorso per concentrarsi su uno più pratico e orientato alla richiesta del mercato del lavoro: la programmazione.",
+    "genere" => "m",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/ortenzi.png",
+    "nome" => "Daniela Ortenz",
+    "eta" => 27,
+    "azienda" => "RDS",
+    "ruolo" => "web developer",
+    "descrizione" => "Laureata in psicologia alla Sapienza di Roma con una nascente carriere nella SEO, Daniela si iscrive al corso Boolean. Ora lavora come web developer presso RDS Radio Dimensione Suono.",
+    "genere" => "f",
+  ],
+  [
+    "img" => "https://www.boolean.careers/images/students/quarticelli.png",
+    "nome" => "Simone Quarticelli",
+    "eta" => 32,
+    "azienda" => "VLC 2",
+    "ruolo" => " web developer",
+    "descrizione" => "Per supportare la famiglia, Simone inizia a lavorare come operaio da giovanissimo. Fortemente motivato a cambiare vita, per 6 mesi non stacca mai lo sguardo dal traguardo, distinguendosi per tenacia e resilienza.",
+    "genere" => "m",
+  ],
+
+]
+
+];
+
+ ?>
---
+++ resources/views/studenti/index.blade.php
@@ -0,0 +1,1 @@
+{{config("studenti.studenti")}}
