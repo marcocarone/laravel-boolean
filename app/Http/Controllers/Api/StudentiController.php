2c0b53447ac30aced3babdbd8ecdb85463b9c908
Marco <marco.carone@libero.it>
2020-03-18 16:35:23 +0100

    up

--- app/Http/Controllers/Api/StudentiController.php
+++ app/Http/Controllers/Api/StudentiController.php
@@ -10,7 +10,19 @@ class StudentiController extends Controller
   public function eta(Request $request)
  {
      $data = $request->all();
-     dd($data);
-     // return response()->json($data);
+     // dd($data);
+
+     $studenti = config('studenti.studenti');
+
+     $result = [
+         'error' => '',
+         'response' => []
+     ];
+
+     // if($data['eta'] > 0 && $data['eta'] <= 25 ) {
+         $result['response'] = $studenti;
+         return response()->json($result);
+     // }
+
  }
 }
